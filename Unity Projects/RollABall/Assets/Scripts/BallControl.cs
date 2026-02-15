286c345936bf4e03a53cbc3383849e2680ba8db7
Damla Gül <damlasaykara@gmail.com>
2026-02-15 20:52:19 +0300

    UnityProjectAdd

---
+++ Unity Projects/RollABall/Assets/Scripts/BallControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    public Rigidbody physics;
+    public int speed;
+    public int counter;
+
+    public Text pointText;
+    public Text gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+
+        Vector3 vektor = new Vector3(yatay, 0, dikey);
+
+        physics.AddForce(vektor*speed);
+
+    }
+    
+    void OnTriggerEnter(Collider other) {
+        other.gameObject.SetActive(false);
+        counter++;
+        pointText.text = "Points: " + counter;
+
+        if(counter == 11) {
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ Unity Projects/RollABall/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject Ball;
+    public Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - Ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Ball.transform.position + distance;
+    }
+}
